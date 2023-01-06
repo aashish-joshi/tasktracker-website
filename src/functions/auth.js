7ef66154fcd69a5b4922ab18e13b93b4056b02fb
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2023-01-06 05:39:49 +0530

    check token

--- src/functions/auth.js
+++ src/functions/auth.js
@@ -14,6 +14,7 @@ export const checkSignIn = () => {
             localStorage.getItem("loggedIn") === "true"
           ) {
             // TODO: check if the token is valid. If not, ask user to sign in again and return True if all is OK.
+            tokenIsValid(localStorage.getItem("access_token"));
             return true;
           }
         return false;
@@ -22,4 +23,15 @@ export const checkSignIn = () => {
     // TODO: Show error message on UI that storge is unavailable.
     console.error('Unable to access browser storage.')
     return false;
+}
+
+export const tokenIsValid = (token) => {
+    if(token){
+        const { sub, email, exp, aud } = decodeJwt(token);
+        console.log(sub, email, exp, aud);
+    }
+}
+
+export function decodeJwt(token) {
+    return JSON.parse(atob(token.split(".")[1]));
 }
